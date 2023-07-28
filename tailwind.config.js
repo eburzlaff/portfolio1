e4bd58b0a53e4605ebedccb89a9253df69cb0e6a
eburzlaff <du.di.99@hotmail.com>
2023-07-28 01:41:22 -0500

    Adding custom font to portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,16 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [
+          'Borel',
+          'sans-serif',
+        ],
+      }
+    },
   },
   plugins: [],
 }
