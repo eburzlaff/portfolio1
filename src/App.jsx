63a3cf7d801d46216fed22a524d5abf735b02892
eburzlaff <du.di.99@hotmail.com>
2023-08-09 12:42:37 -0500

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -34,12 +34,13 @@ function App() {
 	}, [theme]);
 
   return (
-    <div className='min-h-screen w-screen bg-white dark:bg-slate-900 overflow-hidden'>
+    <div className='min-h-screen w-screen bg-white mx-auto dark:bg-slate-900 overflow-hidden'>
       <Header handleThemeSwitch={handleThemeSwitch} theme={theme}/>
 	  <a id='home'></a>
       <div className='max-w-[1280px] mx-auto px-5 lg:px-0'>        
         <Hero/>
 		<hr className='w-20 h-1 mx-auto lg:mr-[100%] my-4 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 to-sky-500 border-0 rounded mt-10 lg:mt-32  '/>	
+		Some
         <About/>
 		<hr className='w-20 h-1 mx-auto lg:mr-[100%] my-4 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 to-sky-500 border-0 rounded mt-10 '/>
         <Skills/>
