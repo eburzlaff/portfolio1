63b8558378bc16c965b15675b0322a8fbf5cfb87
eburzlaff <du.di.99@hotmail.com>
2023-08-10 07:11:08 -0500

    correcting grammar in the about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ function About(props) {
                 <p className='text-slate-700 dark:text-slate-400 text-xl mt-6'>
                 My name is Eduardo, a recent college graduate with a passion for software development. My journey into the coding world began in 2019 when I stumbled upon some online courses. As I dived into web development, I found myself completely captivated by the process and developed a strong interest for it.
 
-                At first, I hoped to pursue a carreer in game development, but I soon realized that starting with web development was a wise move, and I haven't looked back since. My online portfolio showcases my progress and various projects that I've worked on.
+                At first, I hoped to pursue a carreer in game development, but I soon realized that starting with web development was a wise move, and I haven't looked back ever since. My online portfolio showcases my progress and various projects that I've worked on.
 
                 When I'm not coding, you'll often find me enjoying battles in fighting games like Street Fighter and Mortal Kombat. Just like in the digital arena, I find joy in the challenge, learning new strategies, and continuously improving my skills.
 
